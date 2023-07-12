da3177c65db40a06d9dadc21665a7a4c67f61356
karimelkomy <karimelkomy@msn.com>
2023-07-12 14:39:00 +0300

    Create TasksApi

---
+++ apis/TasksApi.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { TodoistApi } from "@doist/todoist-api-typescript";
+
+export default class TasksApi {
+  static async getTasks() {
+    try {
+      const api = new TodoistApi(process.env.API_TOKEN);
+      const response = await api.getTasks();
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async createTask({ taskDetails }) {
+    try {
+      const api = new TodoistApi(process.env.API_TOKEN);
+      const response = await api.addTask({
+        content: taskDetails.taskName,
+        description: taskDetails.taskDescription,
+      });
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
