70f168bbc17b3980410797ee8c739754e2635968
karimelkomy <karimelkomy@msn.com>
2023-07-12 14:04:58 +0300

    Update packages

--- tests/first-test.js
+++
@@ -1,9 +0,0 @@
-import { urls } from "../data/urls";
-
-fixture`First fixture`.page(urls.home).beforeEach(async (t) => {
-  await t.setNativeDialogHandler(() => true);
-});
-
-test("first test", async (t) => {}).meta({
-  customTest: "first-test",
-});
